e58d9ed1001a136bf22f2e0abea4f4528ac8c90f
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-07-24 10:06:52 -0700

    init commit

---
+++ demo/src/main/java/com/example/demo/BookResource.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookResource {
+	//connect book service
+	
+	@Autowired
+	private BookService bookService;
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		return bookService.findAllBooks();
+	}
+	
+	@GetMapping("/books/{bookId}")
+	public Book retriveBook(@PathVariable int bookId) {
+		return bookService.findBook(bookId);
+	}
+	
+	@PostMapping("/books")
+	public void  createBook(@RequestBody Book book) {
+		Book addbook=bookService.addBook(book);
+	}
+}
---
+++ demo/src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookService {
+	
+	//create list
+	private static List<Book> books=new ArrayList<>();
+	
+	//create book count
+	private static int bookCount =3;
+	
+	//add books on the list
+	static {
+		books.add(new Book(1,"Harry Potter",new Date()));
+		books.add(new Book(2,"Twiligth",new Date()));
+		books.add(new Book(3,"Games",new Date()));
+	}
+	
+	//method
+	public List<Book> findAllBooks(){
+		return books;
+	}
+	
+	//method
+	public Book addBook(Book book) {
+		if(book.getBookId()== null) {
+			book.setBookId(bookCount++);
+		}
+		books.add(book);
+		return book;
+	}
+	//method for find book
+	public Book findBook(int id) {
+		for(Book book :books) {
+			if(book.getBookId()==id) {
+				return book;
+			}
+		}
+		return null;
+	}
+}
